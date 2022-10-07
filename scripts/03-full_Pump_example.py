89b78a28b147f41c28bcd3b2f92cb82ef7596e12
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-10-07 15:51:40 +0100

    finished basic elerment example

--- scripts/03-full_Pump_example.py
+++ scripts/03-full_Pump_example.py
@@ -31,35 +31,37 @@ gvxr.createOpenGLContext(window_id, opengl_major_version, opengl_minor_version)
 
 gvxr.setSourcePosition(0.0 ,-40.0,0.0,"cm")
 gvxr.usePointSource()
-gvxr.setMonoChromatic(0.1,"MeV",1000)
-
-gvxr.setDetectorNumberOfPixels(720,576)
-gvxr.setDetectorPixelSize(0.75,0.75,"mm")
+gvxr.setMonoChromatic(0.3,"MeV",1000)
+#gvxr.setMonoChromatic(0.8,"MeV",1000)
+#gvxr.setDetectorNumberOfPixels(720,576)
+gvxr.setDetectorNumberOfPixels(800,800)
+gvxr.setDetectorPixelSize(0.7,0.7,"mm")
 gvxr.setDetectorPosition(0.0,30.0,0.0,"cm")
 gvxr.setDetectorUpVector(0,0,-1)
 
-parts_list = ["internals","shaft","front_plate","back_plate","housing"]
-part_files = ["input_data/TurboPump/Part3.stl",
-               "input_data/TurboPump/Part4.stl",
-               "input_data/TurboPump/Part5.stl",
-               "input_data/TurboPump/Part6.stl",
-               "input_data/TurboPump/Part7.stl"]
+parts_list = ["internals","coupler","front_flange","rear_flange","housing","roller_bearing"]
+part_files = ["input_data/TurboPump/internals.stl",
+               "input_data/TurboPump/coupler.stl",
+               "input_data/TurboPump/front_flange.stl",
+               "input_data/TurboPump/rear_flange.stl",
+               "input_data/TurboPump/housing.stl",
+               "input_data/TurboPump/ThrustRollerBearing.stl"]
 
-materials = ["Fe","Fe","Al","Al","Ti"]
-
+materials = ["Fe","Fe","Al","Al","Al","Fe"]
+#materials = ["W","W","C","C","C","W"]
 for i,name in enumerate(parts_list):
      gvxr.loadMeshFile(name,part_files[i],"mm")
      gvxr.setElement(name, materials[i])
-     gvxr.translateNode(name,0.0,5.0,0.0,"cm")
+     gvxr.translateNode(name,0.0,-20.0,0.0,"cm")
      gvxr.moveToCentre()
-     gvxr.scaleNode(name, 0.8, 0.8, 0.8)
+     #gvxr.scaleNode(name, 0.8, 0.8, 0.8)
      gvxr.displayScene()
 
 xray_image_front = np.array(gvxr.computeXRayImage()).astype(np.single)
 # Create the output directory if needed
 if not os.path.exists("output_data"):
      os.mkdir("output_data")
-# write x-rayimage to file
+# write x-ray image to file
 #gvxr.saveLastXRayImage('output_data/03-Pump_front.tiff')
 imwrite('output_data/03-Pump_front.tiff',xray_image_front)
 
@@ -69,6 +71,6 @@ for i,name in enumerate(parts_list):
      gvxr.rotateNode(name, 90, 1, 0, 0)
 xray_image_side = np.array(gvxr.computeXRayImage()).astype(np.single)
 #gvxr.saveLastXRayImage('output_data/03-Pump_side.tiff')
-#imwrite('output_data/03-Pump_front.tiff',xray_image_front)
-gvxr.renderLoop()
+imwrite('output_data/03-Pump_side.tiff',xray_image_side)
+#gvxr.renderLoop()
 gvxr.terminate()
